814395d20734c799284229114e3835a497af43ce
domezy <domezy@gmail.com>
2017-12-13 21:39:57 +0700

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,20 +10,20 @@ touch /opt/cacti/log/cacti.log
 chown -R www-data:www-data /opt/cacti/
 
 #Initial conf for mysql
-mysql_install_db
+#mysql_install_db
 #for configuriing database
-/usr/bin/mysqld_safe &
+#/usr/bin/mysqld_safe &
 
- sleep 3s 
+ #sleep 3s 
   
- mysqladmin -u root password mysqlpsswd
- mysqladmin -u root -pmysqlpsswd reload
- mysqladmin -u root -pmysqlpsswd create cacti
- echo "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
- echo "GRANT SELECT ON mysql.time_zone_name TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
+ #mysqladmin -u root password mysqlpsswd
+ #mysqladmin -u root -pmysqlpsswd reload
+ #mysqladmin -u root -pmysqlpsswd create cacti
+ #echo "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
+ #echo "GRANT SELECT ON mysql.time_zone_name TO cacti@localhost IDENTIFIED BY '9PIu8AbWQSf8'; flush privileges; " | mysql -u root -pmysqlpsswd 
  
- mysql -u root -pmysqlpsswd cacti < /opt/cacti/cacti.sql
- mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -pmysqlpsswd mysql
+ #mysql -u root -pmysqlpsswd cacti < /opt/cacti/cacti.sql
+ #mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -pmysqlpsswd mysql
  
  
  #to fix error relate to ip address of container apache2
@@ -36,25 +36,25 @@ mysql_install_db
  #configure poller Crontab
  echo "*/5 * * * * www-data php /opt/cacti/poller.php > /dev/null 2>&1" >> /etc/crontab 
 
-killall mysqld
+#killall mysqld
 sleep 2s
 
-mv /etc/mysql/my.cnf /etc/mysql/my.cnf-bkup
-echo "
-[mysqld]
+#mv /etc/mysql/my.cnf /etc/mysql/my.cnf-bkup
+#echo "
+#[mysqld]
 
-max_heap_table_size = 1073741824
-max_allowed_packet = 16777216
-tmp_table_size = 134217728
-join_buffer_size = 134217728
-innodb_buffer_pool_size = 4294967296
-innodb_doublewrite = OFF
-innodb_flush_log_at_timeout = 10
-innodb_read_io_threads = 32
-innodb_write_io_threads = 16
-collation-server = utf8mb4_unicode_ci
-character-set-server = utf8mb4
-" > /etc/mysql/my.cnf
+#max_heap_table_size = 1073741824
+#max_allowed_packet = 16777216
+#tmp_table_size = 134217728
+#join_buffer_size = 134217728
+#innodb_buffer_pool_size = 4294967296
+#innodb_doublewrite = OFF
+#innodb_flush_log_at_timeout = 10
+#innodb_read_io_threads = 32
+#innodb_write_io_threads = 16
+#collation-server = utf8mb4_unicode_ci
+#character-set-server = utf8mb4
+#" > /etc/mysql/my.cnf
 
 cd /opt/
 wget http://www.cacti.net/downloads/spine/cacti-spine-latest.tar.gz
